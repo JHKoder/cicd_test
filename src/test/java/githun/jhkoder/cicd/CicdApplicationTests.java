def77b6ea02db735351750a51a468b88b52e495c
JHKoder <jeonghun.kang.dev@gmail.com>
2024-08-29 21:53:06 +0900

    init

---
+++ src/test/java/githun/jhkoder/cicd/CicdApplicationTests.java
@@ -0,0 +1,14 @@
+package githun.jhkoder.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("--test ");
+	}
+
+}
